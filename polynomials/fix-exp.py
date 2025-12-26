33bce915fb17de2c203b813cbfe7ca5c28f891d8
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2025-12-25 19:40:48 -0500

    refactor: replace recursive Fibonacci implementation with iterative approach

--- polynomials/fix-exp.py
+++ polynomials/fix-exp.py
@@ -1,7 +1,23 @@
+# def fib(n):
+#     if n == 0:
+#         return 0
+#     if n == 1:
+#         return 1
+#     return fib(n - 1) + fib(n - 2)
+
 def fib(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fib(n - 1) + fib(n - 2)
+    # n = 4
+    # 0 1 1 2 3
+    a = 0
+    b = 1
+    result = n
+
+    for _ in range(n - 1):
+        result = a + b
+        a = b
+        b = result
+    return result
+
+print(fib(10))
+
 
