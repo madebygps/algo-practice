b575ba238251982f68e88dd9d2c787b9187e7d63
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2025-12-26 17:14:18 -0500

    fix: use factor parameter in exponential_growth function for correct calculations

--- polynomials/growth_sequence.py
+++ polynomials/growth_sequence.py
@@ -1,7 +1,7 @@
 def exponential_growth(n, factor, days):
     growth_sequence = [n]
     for i in range(days):
-        growth_sequence.append(growth_sequence[i] * 2)
+        growth_sequence.append(growth_sequence[i] * factor)
     
     
     return growth_sequence
